29148f484fe3c453b29905e5724f3d1b207647ce
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-05-22 10:01:04 +0000

    nagano_cake customers and admins migrate

--- db/migrate/20220522094605_devise_create_customers.rb
+++ db/migrate/20220522094605_devise_create_customers.rb
@@ -6,6 +6,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name, null: false, default: ""
+      t.string :first_name, null: false, default: ""
+      t.string :last_name_kana, null: false, default: ""
+      t.string :first_name_kana, null: false, default: ""
+      t.string :postal_code, null: false, default: ""
+      t.string :address, null: false, default: ""
+      t.string :telephone_number, null: false, default: ""
+      t.boolean :is_active, null:false, default: true
 
       ## Recoverable
       t.string   :reset_password_token
