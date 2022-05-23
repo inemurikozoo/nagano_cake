1e61d8b3dceb626edac66df4181cacef7ee69d4d
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-05-23 03:00:11 +0000

    nagano_cake model made!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.active_job.queue_adapter = :inline
+  config.hosts << "dcbc6cbb167d4cf38c18aedca3c7b4dd.vfs.cloud9.us-east-1.amazonaws.com"
 end
