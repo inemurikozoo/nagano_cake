99baa659adcbc2ef6df2117f2f1d4f4ded742025
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-05-23 11:44:25 +0000

    nagano_cake controller making.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 
-#顧客用
+#会員用のルーティング設定
+  get 'items' => 'public/items#index'
+#管理者用のルーティング設定
+namespace :admin do
+    get 'items' => 'admin/items#index'
+end
+
+#会員用
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
 #管理者用
-  devise_for :admin,skip: [:registrations, :passwords] ,controllers: {
+    devise_for :admin,skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
 
