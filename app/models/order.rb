cea22e2c852289ce176d09417e1270e64afa3fe0
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-05-27 00:21:13 +0000

    nagano_cake header making.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   enum payment_method: {credit_card: 0, transfer: 1 }
   enum status: {not_payment: 0, payment_comfirm: 1, producting: 2,　ready_to_ship: 3, sent: 4}
+  has_many :order_items, dependent: :destroy
+  belongs_to :customer
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,5 @@
 class OrderItem < ApplicationRecord
   enum status: {not_startable: 0, waiting: 1, producting: 2, complete: 3}
+  belongs_to :order
+  belongs_to :item
 end
