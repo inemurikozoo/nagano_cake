77161babdef449530b1d5a4abf32804e846ce3e0
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-06-07 13:31:23 +0000

    admin/item/show making.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   enum pay_type: {credit_card: 0, transfer: 1 }
-  enum status: {not_payment: 0, payment_comfirm: 1, producting: 2,　ready_to_ship: 3, sent: 4}
+  enum status: {not_payment: 0, payment_comfirm: 1, producting: 2, ready_to_ship: 3, sent: 4}
   
   has_many :order_items, dependent: :destroy
   belongs_to :customer
