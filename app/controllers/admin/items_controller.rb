fce4d86c49d5c2fd056c05746f20a39b09c3751b
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-06-09 06:23:16 +0000

    admin/item/edit making.

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,14 @@
 class Admin::ItemsController < ApplicationController
+before_action :set_genres, only: [:new,:create,:edit,:update]
+
+
+  def new
+    @item = Item.new
+  end
+
   def index
     @items = Item.page(params[:page])
   end
-
 
   def show
     @item = Item.find(params[:id])
@@ -12,8 +18,25 @@ class Admin::ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path(@item.id)
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update
+    redirect_to admin_items_path(@item.id)
+  end
+
+
   private
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :is_active)
+  end
+
+  def set_genres
+    @genres = Genre.all
   end
 end
