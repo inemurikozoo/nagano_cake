cb75fc132d1b9143a5e971a902ecc6083dc96af6
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-06-20 23:09:44 +0000

    public making start!

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only[:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,10 +19,22 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  #退会しているかを判断するメソッド
+  def customer_state
+    @customer = Customer.find_by(email :params[:cusomer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_active == true
+      redirect_to :create  
+    else
+      redirect_to customer_session_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  
 end
